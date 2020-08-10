87f254a59dbc970cfeee9e8fae9b0a8296c320e2
Anisio Mandlate <anisiomandlate@gmail.com>
2020-08-10 12:00:26 +0200

    feat: Add submit button and fix breakpoints

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    console.log(
+      `Hey: ${firstName} ${lastName}. Your email is ${email} and password: ${password}`
+    );
+  }
+
   return (
     <div className="App">
-      <form>
+      <form onSubmit={handleSubmit}>
         <fieldset>
           <legend>
             <h2>Dados</h2>
@@ -12,22 +25,55 @@ function App() {
           <div className="field-group">
             <div className="field">
               <label htmlFor="firstName">First Name</label>
-              <input type="text" name="firstName" id="fisrtName" />
+              <input
+                type="text"
+                name="firstName"
+                id="fisrtName"
+                value={firstName}
+                onChange={(e) => {
+                  setFirstName(e.target.value);
+                }}
+              />
             </div>
             <div className="field">
               <label htmlFor="lastName">Last Name</label>
-              <input type="text" name="lastName" id="lastName" />
+              <input
+                type="text"
+                name="lastName"
+                id="lastName"
+                value={lastName}
+                onChange={(e) => {
+                  setLastName(e.target.value);
+                }}
+              />
             </div>
           </div>
           <div className="field">
             <label htmlFor="email">Email</label>
-            <input type="email" name="email" id="email" />
+            <input
+              type="email"
+              name="email"
+              id="email"
+              value={email}
+              onChange={(e) => {
+                setEmail(e.target.value);
+              }}
+            />
           </div>
           <div className="field">
             <label htmlFor="password">Password</label>
-            <input type="password" name="password" id="password" />
+            <input
+              type="password"
+              name="password"
+              id="password"
+              value={password}
+              onChange={(e) => {
+                setPassword(e.target.value);
+              }}
+            />
           </div>
         </fieldset>
+        <button type="submit">Submit</button>
       </form>
     </div>
   );
